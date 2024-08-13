2a2920ab6467e60e03f54185508848daf7536ce0
utyvgj1afe <DahnkeCoggin@gmail.com>
2024-08-13 18:38:32 +0800

    Create winter.java

---
+++ winter.java
@@ -0,0 +1,39 @@
+public class Animal { 
+    private String name;  
+    private int id; 
+    public Animal(String myName, int myid) { 
+        name = myName; 
+        id = myid;
+    } 
+    public void eat(){ 
+        System.out.println(name+"正在吃"); 
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡");
+    }
+    public void introduction() { 
+        System.out.println("大家好！我是"         + id + "号" + name + "."); 
+    } 
+}
+public class Penguin extends Animal { 
+    public Penguin(String myName, int myid) { 
+        super(myName, myid); 
+    } 
+}
+public class Mouse extends Animal { 
+    public Mouse(String myName, int myid) { 
+        super(myName, myid); 
+    } 
+}
+public class Animal { 
+    private String name;   
+    private int id; 
+    public Animal(String myName, int myid) { 
+        //初始化属性值
+    } 
+    public void eat() {  //吃东西方法的具体实现  } 
+    public void sleep() { //睡觉方法的具体实现  } 
+} 
+ 
+public class Penguin  extends  Animal{ 
+}
